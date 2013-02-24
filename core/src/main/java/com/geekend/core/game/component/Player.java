6eb69b83cd1673a3b982c1c17320b12cbbe3796d
rhlobo <rhlobo+github@gmail.com>
2013-02-24 04:05:01 -0300

    Sprites are working seamessly.

--- core/src/main/java/com/geekend/core/game/component/Player.java
+++ core/src/main/java/com/geekend/core/game/component/Player.java
@@ -1,43 +1,89 @@
 package com.geekend.core.game.component;
 
-import static playn.core.PlayN.assets;
-import static playn.core.PlayN.graphics;
 import playn.core.GroupLayer;
-import playn.core.Image;
-import playn.core.ImageLayer;
 import playn.core.Key;
-import playn.core.util.Callback;
+import playn.core.PlayN;
+import playn.core.ResourceCallback;
 
 import com.geekend.core.game.input.InputOracle;
+import com.geekend.core.game.sprite.Sprite;
+import com.geekend.core.game.sprite.SpriteLoader;
 
+@SuppressWarnings("deprecation")
 public class Player {
 
-	private static final String IMAGE_PLAYER_GIF = "images/player.gif";
+	private enum PlayerStates {
+		WAITING(new int[] { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 2, 2, 0, 1, 0, 1, 0, 56, 56 }, 800f), SHOOTING(new int[] { 3, 4, 5, 6, 7 }, 40f), RUNNING(
+				new int[] { 8, 9, 10, 11, 12, 13, 14, 15 },
+				120f), SKATING(new int[] { 16, 17 }, 300f), SKATING_HIT(new int[] { 22 }, 50f), HIT(new int[] { 53 }, 50f), DEAD(new int[] { 55 }, 50f);
 
+		private final int[] spriteIndexes;
+		private final float delay;
+
+		private PlayerStates(final int[] spriteIndexes, final float delay) {
+			this.spriteIndexes = spriteIndexes;
+			this.delay = delay;
+		}
+
+		public static PlayerStates determinePlayerState() {
+			if (InputOracle.isKeyPressed(Key.F)) return PlayerStates.SHOOTING;
+			else if (InputOracle.isKeyPressed(Key.UP) || InputOracle.isKeyPressed(Key.DOWN)) return InputOracle.isKeyPressed(Key.TAB) ? PlayerStates.SKATING
+					: PlayerStates.RUNNING;
+			else
+				return PlayerStates.WAITING;
+		}
+
+		public float getCountdownDelay() {
+			return delay;
+		}
+
+		public int numSprites() {
+			return spriteIndexes.length;
+		}
+
+		public int getSpriteIndex(final int index) {
+			return spriteIndexes[index];
+		}
+	}
+
+	private static final String IMAGE = "sprites/player.png";
+	private static final String JSON = "sprites/player.json";
+
+	private static final int SPRITE_HEIGHT = 16;
+	private static final int SPRITE_WIDTH = 16;
 	private static final float SPEED = 0.13f;
 	private static final float TURNING_SPEED = 1f / 200f;
+
 	private float x = 0;
 	private float y = 0;
 	private float angle = 0;
+	private PlayerStates state = PlayerStates.WAITING;
 
 	private GroupLayer mainLayer;
-	private ImageLayer imageLayer;
+	private Sprite playerSprite;
+	private float spriteCountdown;
+	private int spriteIndex;
 
 	public void init(final GroupLayer layer, final int i, final int j) {
 		mainLayer = layer;
-		final Image image = assets().getImage(IMAGE_PLAYER_GIF);
-		image.addCallback(new Callback<Image>() {
+		playerSprite = SpriteLoader.getSprite(IMAGE, JSON);
+		playerSprite.addCallback(new ResourceCallback<Sprite>() {
 
 			@Override
-			public void onSuccess(final Image result) {
-				imageLayer.setOrigin(imageLayer.width() / 2, imageLayer.height() / 2);
+			public void done(final Sprite resource) {
+				playerSprite.layer().setOrigin(SPRITE_WIDTH, SPRITE_HEIGHT);
 			}
 
 			@Override
-			public void onFailure(final Throwable cause) {}
+			public void error(final Throwable err) {
+				final Console console = new Console();
+				console.init(PlayN.graphics().rootLayer());
+				console.log("erro!!!!!!!!");
+				console.log(err.getMessage());
+				console.log(err.toString());
+			}
 		});
-		imageLayer = graphics().createImageLayer(image);
-		mainLayer.add(imageLayer);
+		mainLayer.add(playerSprite.layer());
 	}
 
 	public float getSpeed() {
@@ -63,10 +109,35 @@ public class Player {
 		if (InputOracle.isKeyPressed(Key.RIGHT)) angle += delta * getTurningSpeed();
 		x += Math.cos(angle) * distance * direction;
 		y += Math.sin(angle) * distance * direction;
+
+		if (!playerSprite.isReady()) return;
+
+		updatePlayerState();
+		if (spriteCountdown < 0) updateSprite();
+		else
+			spriteCountdown -= delta;
+
+	}
+
+	private void updatePlayerState() {
+		final PlayerStates current = state;
+		state = PlayerStates.determinePlayerState();
+
+		if (state == current) return;
+
+		spriteIndex = -1;
+		spriteCountdown = state.getCountdownDelay();
+		updateSprite();
+	}
+
+	private void updateSprite() {
+		spriteIndex = (spriteIndex + 1) % state.numSprites();
+		playerSprite.setSprite(state.getSpriteIndex(spriteIndex));
+		spriteCountdown = state.getCountdownDelay();
 	}
 
 	public void paint(final float alpha) {
-		imageLayer.setTranslation(x, y);
-		imageLayer.setRotation(angle);
+		playerSprite.layer().setTranslation(x, y);
+		playerSprite.layer().setRotation(angle);
 	}
 }
